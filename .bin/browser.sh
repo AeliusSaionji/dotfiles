837219772262d5dee74c1376381ae0c2f6fb22a6
Aelius <link68759@hotmail.com>
2014-03-14 02:15:16 -0400

    changed a lot

--- .bin/browser.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-[[ -z $((xprop -id $(</tmp/tabbed.xid)) 2>/dev/null) ]] && tabbed -cd > /tmp/tabbed.xid 
-exec vimprobable2 -e $(</tmp/tabbed.xid) $( [[  "$1"  ]] && echo "$1") &
--- .vimperator/plugin/TineyeImageSearchHint.js
+++
@@ -1,8 +0,0 @@
-hints.addMode(
-    'e',
-    'Tineye Image Search',
-	function (elem) {
-		liberator.open('http://www.tineye.com/search/?pluginver=firefox-1.0&url=' + encodeURIComponent(elem.src), liberator.NEW_TAB);
-	},
-	function () { return "//*[@src]"; }
-);
