ab94e482ec28b629c1dfdd985c4a0cdb03c7fc97
Link Satonaka <link-satonaka@outlook.com>
2015-05-07 18:20:26 -0400

    if the folder exists, it will be used, no need to manually add entries for new computers

--- restore.sh
+++ restore.sh
@@ -1,7 +1,5 @@
 rsync --exclude-from="exclude" -avhP --no-o --no-g . /
 host="$(hostnamectl status --static)"
-if [ $host = "GIR" ]; then
-	rsync --avhP --no-o --no-g ./GIR/ /
-elif [ $host = "Saya" ]; then
-	rsync --avhP --no-o --no-g ./Saya/ /
+if [ -d ./$host ]; then
+	rsync --avhP --no-o --no-g ./$host/ /
 fi
