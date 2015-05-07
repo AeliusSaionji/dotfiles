7b8714876406e50e07b1d0e8deae788b46efa2e9
Link Satonaka <link-satonaka@outlook.com>
2015-05-07 18:03:56 -0400

    create a folder for every machine, no sharing etc

--- backup.sh
+++ backup.sh
@@ -1,1 +1,3 @@
 rsync -avhP --log-file="/home/Link/changes" --delete --files-from="include" --exclude-from="exclude" --delete-excluded / .
+host="$(hostnamectl status --static)"
+rsync -avhP --no-g --no-o --delete --files-from"machineconfig" ./$host/
