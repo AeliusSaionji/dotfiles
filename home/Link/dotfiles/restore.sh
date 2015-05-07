49e7c66e2c7409846fd2b585f2f960197c62ad34
Link Satonaka <link-satonaka@outlook.com>
2015-05-07 18:17:17 -0400

    probably working etc per machine now?

--- home/Link/dotfiles/restore.sh
+++ home/Link/dotfiles/restore.sh
@@ -1,1 +1,7 @@
 rsync --exclude-from="exclude" -avhP --no-o --no-g . /
+host="$(hostnamectl status --static)"
+if [ $host = "GIR" ]; then
+	rsync --avhP --no-o --no-g ./GIR/ /
+elif [ $host = "Saya" ]; then
+	rsync --avhP --no-o --no-g ./Saya/ /
+fi
