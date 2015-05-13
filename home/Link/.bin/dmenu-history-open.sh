295b383434b399503077cc8cffdd63b62e316061
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-12 21:07:18 -0400

    removed some bad cats from browser scripts, added talim to dwm-personalized

--- home/Link/.bin/dmenu-history-open.sh
+++ home/Link/.bin/dmenu-history-open.sh
@@ -1,1 +1,1 @@
-cat ~/.xombrero/history | sed '/^http/!d' | dmenu -f -i -l 10 -p 'history search' | xargs -0r xomb-open.sh
+sed '/^http/!d' ~/.xombrero/history | dmenu -f -i -l 10 -p 'history search' | xargs -0r xomb-open.sh
