8c8c36370e10350f95c082b4931d9f0edf4a8474
Link <link-satonaka@outlook.com>
2015-05-15 13:06:29 -0400

    brightness indicator for saya

---
+++ Saya/home/Link/.mmkeyosd/brightup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xbacklight -inc 10
+BRFILE=/sys/class/backlight/intel_backlight/brightness
+BRMAXFILE=/sys/class/backlight/intel_backlight/max_brightness
+echo `cat $BRFILE` `cat $BRMAXFILE` | awk 'END{print int($1/$2*100)}'
+
