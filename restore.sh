2039700bcd54544d1598135020d35a0e2ed68a7e
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-15 13:22:04 -0400

    fixed machine/user restore

--- restore.sh
+++ restore.sh
@@ -2,5 +2,5 @@ rsync -avhP --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/
 host="$(hostnamectl status --static)"
 if [ -d ./$host ]; then
 	sudo rsync -avhP --chown=root:root --exclude="home" ./$host/ /
-	rsync -avhP --chown=Link:Link ./$host/home/Link /home/Link
+	rsync -avhP --chown=Link:Link ./$host/home/Link/ /home/Link/
 fi
