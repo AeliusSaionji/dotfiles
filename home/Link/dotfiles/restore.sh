7bee16b84a5254c0c2ddd9ea57977ff67ae89ff7
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-15 12:57:17 -0400

    added ability to have per machine user settings

--- home/Link/dotfiles/restore.sh
+++ home/Link/dotfiles/restore.sh
@@ -1,5 +1,6 @@
 rsync -avhP --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/
 host="$(hostnamectl status --static)"
 if [ -d ./$host ]; then
-	sudo rsync -avhP --chown=root:root ./$host/ /
+	sudo rsync -avhP --chown=root:root --exclude="home" ./$host/ /
+	rsync -avhP --chown=Link:Link ./$host/home/Link /home/Link
 fi
