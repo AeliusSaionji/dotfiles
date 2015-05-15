2a6c55ec5e140d089b3bc00a563bcac56e72de0c
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-15 01:14:13 -0400

    made mmkeyosd redundant by having the dwmstatus update in real time. maybe revert to just having dwm control volume

---
+++ home/Link/.bin/dwmstatus-trigger-update.sh
@@ -0,0 +1,21 @@
+# Power/Battery Status
+if [ "$( cat /sys/class/power_supply/AC/online )" -eq "1" ]; then
+        DWM_BATTERY="AC";
+        DWM_RENEW_INT=3;
+else
+        DWM_BATTERY=$(( `cat /sys/class/power_supply/BAT0/charge_now` * 100 / `cat /sys/class/power_supply/BAT0/charge_full` ));
+        DWM_RENEW_INT=30;
+fi;
+
+# Volume Level
+DWM_VOL=$( amixer sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
+
+# Network
+NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/\* //')
+
+# Date and Time
+DWM_CLOCK=$( date '+%H:%M' );
+
+# Overall output command
+DWM_STATUS="[$DWM_BATTERY] | $DWM_VOL| $NETPROFILE | $DWM_CLOCK";
+xsetroot -name "$DWM_STATUS";
