a41244ee8573df941caf8b00aa0ff91679b10545
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-16 21:54:45 -0400

    irssi scripts and the config initializer

---
+++ home/Link/.irssi/link-autorun.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cd scripts/autorun
+ln -s ../adv_windowlist.pl .
+ln -s ../autorun-list .
+ln -s ../awaylogcnt.pl .
+ln -s ../clearable.pl .
+ln -s ../colorize_nicks.pl .
+ln -s ../dccstat.pl .
+ln -s ../digraphs.pl .
+ln -s ../dim_nicks.pl .
+ln -s ../emojis.pl .
+ln -s ../hideshow.pl .
+ln -s ../hilightwin.pl .
+ln -s ../hlscroll.pl .
+ln -s ../logresume.pl .
+ln -s ../nickbar.pl .
+ln -s ../nickcolor_expando.pl .
+ln -s ../recentdepart.pl .
+ln -s ../repeat.pl .
+ln -s ../romaji.pl .
+ln -s ../sb_position.pl .
+ln -s ../send_scroll.pl .
+ln -s ../timer.pl .
+ln -s ../typofix.pl .
+ln -s ../uberprompt.pl .
+ln -s ../vim_mode.pl .
+ln -s ../windowtidy.pl .
+ln -s ../wpm.pl .
---
+++ home/Link/.irssi/synclogs.bat
@@ -0,0 +1,3 @@
+C:\cygwin64\bin\rsync.exe -am --delete rsync://10.8.0.1/irclogs/Freenode/*.log ~/irclogs/Freenode/
+exit
+REM C:\cygwin64\bin\mintty.exe -i /Cygwin-Terminal.ico -t irssi --class irssiclass -e c:\cygwin64\bin\irssi
