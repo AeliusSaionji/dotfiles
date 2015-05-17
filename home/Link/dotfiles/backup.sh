a90f05e373cf8b3202eaac5809848df09b447bbf
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-17 15:21:15 -0400

    exclude netctl

--- home/Link/dotfiles/backup.sh
+++ home/Link/dotfiles/backup.sh
@@ -1,3 +1,3 @@
 rsync -avhP --chown=Link:Link --log-file="/home/Link/changes" --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
 host="$(hostnamectl status --static)"
-rsync -avhP --delete --files-from="machineconfig" / ./$host/
+rsync -avhP --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/
