16fa5e51a9b462acca83c293cdcb50ed8babca44
Link <link-satonaka@outlook.com>
2015-05-18 12:33:17 -0400

    cute script, but entirely unnecessary

--- home/Link/.irssi/synclogs.sh
+++ home/Link/.irssi/synclogs.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
 timestamp=$(date +%Y-%m)
-if [ "$OSTYPE" = "cygwin" ]; then
-	prefix="/usr/bin/"
-else
-	prefix=""
-fi
-${prefix}rsync -am --no-o --no-g --no-super --delete rsync://10.8.0.1/irclogs/Freenode/*-${timestamp}.log ~/irclogs/Freenode/
+/usr/bin/rsync -am --no-o --no-g --no-super --delete rsync://10.8.0.1/irclogs/Freenode/*-${timestamp}.log ~/irclogs/Freenode/
