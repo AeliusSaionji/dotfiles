d8145045aafbd0b8897b30d34f3998ae939fad95
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-19 01:09:07 -0400

    implemented dry run & approve

--- backup.sh
+++ backup.sh
@@ -1,3 +1,16 @@
-rsync -avhP --chown=Link:Link --log-file="/home/Link/changes" --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
+# separate machineconfig home and root backup
+# make output prettier
+rsync -avn --chown=Link:Link --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+echo "home to global home?"
+echo "Type yes to continue" && read proceed
+if [ "$proceed" = "yes" ]; then
+	rsync -a --ignore-missing-args --chown=Link:Link  --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
+fi
+proceed=""
 host="$(hostnamectl status --static)"
-rsync -avhP --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/
+rsync -avn --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+echo "root to $host root backup?"
+echo "Type yes to continue" && read proceed
+if [ "$proceed" = "yes" ]; then
+	rsync -a --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/
+fi
--- home/Link/dotfiles/restore.sh
+++ home/Link/dotfiles/restore.sh
@@ -1,6 +1,25 @@
-rsync -avhP --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/
+#switch for each, run with delete/without/don't run
+#improve sed regex (numbers vs .*)
+rsync -avn --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+echo "global home > home?"
+echo "Type yes to continue." && read proceed
+if [ "$proceed" = "yes" ]; then
+	rsync -a --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/
+fi
 host="$(hostnamectl status --static)"
 if [ -d ./$host ]; then
-	sudo rsync -avhP --chown=root:root --exclude="home" ./$host/ /
-	rsync -avhP --chown=Link:Link ./$host/home/Link/ /home/Link/
+	proceed=""
+	rsync -avn --chown=Link:Link ./$host/home/Link/ /home/Link/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+	echo "$host home > home?"
+	echo "Type yes to continue." && read proceed
+	if [ "$proceed" = "yes" ]; then
+		rsync -a --chown=Link:Link ./$host/home/Link/ /home/Link/
+	fi
+	proceed=""
+	rsync -avn --chown=root:root --exclude="home" ./$host/ / | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+	echo "$host root > root?"
+	echo "Type yes to continue." && read proceed
+	if [ "$proceed" = "yes" ]; then
+		sudo rsync -a --chown=root:root --exclude="home" ./$host/ /
+	fi
 fi
