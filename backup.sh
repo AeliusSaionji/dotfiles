79a2933a884adedf4639da5ac2af5b79e7856ac6
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-19 12:34:23 -0400

    prettier output

--- backup.sh
+++ backup.sh
@@ -1,16 +1,18 @@
 # separate machineconfig home and root backup
-# make output prettier
+NC='\033[0m' # No Color
+color='\033[1;35m'
 rsync -avn --chown=Link:Link --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
-echo "home to global home?"
-echo "Type yes to continue" && read proceed
+printf "${color}home > global home backup? (yes/no) : ${NC}"
+read proceed
 if [ "$proceed" = "yes" ]; then
 	rsync -a --ignore-missing-args --chown=Link:Link  --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
 fi
 proceed=""
 host="$(hostnamectl status --static)"
 rsync -avn --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+printf "${color}root > ${host} root backup? (yes/no) : ${NC}"
 echo "root to $host root backup?"
-echo "Type yes to continue" && read proceed
+read proceed
 if [ "$proceed" = "yes" ]; then
 	rsync -a --delete --exclude-from="exclude" --files-from="machineconfig" / ./$host/
 fi
--- home/Link/dotfiles/restore.sh
+++ home/Link/dotfiles/restore.sh
@@ -1,8 +1,10 @@
 #switch for each, run with delete/without/don't run
 #improve sed regex (numbers vs .*)
+NC='\033[0m' # No Color
+color='\033[1;35m'
 rsync -avn --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
-echo "global home > home?"
-echo "Type yes to continue." && read proceed
+printf "${color}global home > home? (yes/no) : ${NC}"
+read proceed
 if [ "$proceed" = "yes" ]; then
 	rsync -a --chown=Link:Link --exclude-from="exclude" ./home/Link/ /home/Link/
 fi
@@ -10,15 +12,15 @@ host="$(hostnamectl status --static)"
 if [ -d ./$host ]; then
 	proceed=""
 	rsync -avn --chown=Link:Link ./$host/home/Link/ /home/Link/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
-	echo "$host home > home?"
-	echo "Type yes to continue." && read proceed
+	printf "${color}${host} home > home? (yes/no) : ${NC}"
+	read proceed
 	if [ "$proceed" = "yes" ]; then
 		rsync -a --chown=Link:Link ./$host/home/Link/ /home/Link/
 	fi
 	proceed=""
 	rsync -avn --chown=root:root --exclude="home" ./$host/ / | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
-	echo "$host root > root?"
-	echo "Type yes to continue." && read proceed
+	printf "${color}${host} root > root? (yes/no) : ${NC}"
+	read proceed
 	if [ "$proceed" = "yes" ]; then
 		sudo rsync -a --chown=root:root --exclude="home" ./$host/ /
 	fi
