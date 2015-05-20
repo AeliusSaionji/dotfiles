c9e075512cfdfc30e9d7c84baada92b507f23a9d
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-20 17:28:13 -0400

    symlink safe

--- backup.sh
+++ backup.sh
@@ -1,11 +1,12 @@
 # separate machineconfig home and root backup
 color='\033[1;35m'
 NC='\033[0m' # No Color
-rsync -avn --chown=Link:Link --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
+# the following is depreciated, back up all machines then remove it in favor of symlinking
+rsync -aLvn --chown=Link:Link --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/ | sed -e '/^sent .* bytes.*bytes\/sec$/d' -e '/^total size is/d'
 printf "${color}home > global home backup? (yes/no) : ${NC}"
 read proceed
 if [ "$proceed" = "yes" ]; then
-	rsync -a --ignore-missing-args --chown=Link:Link  --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
+	rsync -aL --ignore-missing-args --chown=Link:Link  --delete --files-from="include" --exclude-from="exclude" --delete-excluded /home/ ./home/
 fi
 proceed=""
 host="$(hostnamectl status --static)"
