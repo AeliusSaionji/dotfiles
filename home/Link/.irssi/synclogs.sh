ed66cacf97fc0e572d87ba1fbccc5e565b287c6b
Link Satonaka <Link-Satonaka@outlook.com>
2015-05-20 20:20:57 -0400

    more

--- home/Link/.irssi/synclogs.sh
+++ home/Link/.irssi/synclogs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 timestamp=$(date +%Y-%m)
+echo "/set autolog off" > ~/.irssi/remote-control
 /usr/bin/rsync -am --no-o --no-g --no-super --delete rsync://10.8.0.1/irclogs/Freenode/*-${timestamp}.log ~/irclogs/Freenode/ && printf "logs synced\n" && echo "/set autolog on" > ~/.irssi/remote-control
