c6311595b7a0690c9dcc7661e9cde83038c13898
Link Satonaka <link.satonaka@outlook.com>
2015-08-24 15:19:48 -0400

    spare battery

--- home/Link/.bin/dwmstatus-trigger-update.sh
+++ home/Link/.bin/dwmstatus-trigger-update.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
-# Power/Battery Status
+
+# Power Status
 if [[ "$( cat /sys/class/power_supply/AC/online )" -eq "1" || "$( cat /sys/class/power_supply/ACAD/online )" -eq "1" ]]; then
         POWERSOURCE="+";
 else
         POWERSOURCE="-";
 fi;
+
+# Battery Status
 BATTERYLEVEL=$(( `cat /sys/class/power_supply/BAT0/charge_now` * 100 / `cat /sys/class/power_supply/BAT0/charge_full` ));
+if [ -d /sys/class/power_supply/BAT1 ]; then
+	SPAREBATTERYLEVEL=$(( `cat /sys/class/power_supply/BAT1/charge_now` * 100 / `cat /sys/class/power_supply/BAT1/charge_full` ));
+fi;
 
-# Volume Level
-#VOLUME=$( amixer sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
-
-# Network
-NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/^\* //')
+# Network Status
+NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/^\* //');
 
 # Date and Time
 CLOCK=$( date '+%H:%M' );
 
 # Overall output command
-#DWM_STATUS="$NETPROFILE | [$POWERSOURCE][$BATTERYLEVEL] | $VOLUME| $CLOCK";
-DWM_STATUS="$NETPROFILE | [$POWERSOURCE][$BATTERYLEVEL] | $CLOCK";
+DWM_STATUS="$NETPROFILE | [$SPAREBATTERYLEVEL][$POWERSOURCE][$BATTERYLEVEL] | $CLOCK";
 xsetroot -name "$DWM_STATUS";
--- home/Link/.bin/dwmstatus.sh
+++ home/Link/.bin/dwmstatus.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 DWM_REFRESH_INT="30s"
 while true; do
-
-# Power/Battery Status
-
-if [[ "$( cat /sys/class/power_supply/AC/online )" -eq "1" || "$( cat /sys/class/power_supply/ACAD/online )" -eq "1" ]]; then
-        POWERSOURCE="+";
-else
-        POWERSOURCE="-";
-fi;
-BATTERYLEVEL=$(( `cat /sys/class/power_supply/BAT0/charge_now` * 100 / `cat /sys/class/power_supply/BAT0/charge_full` ));
-
-# Volume Level
-#VOLUME=$( amixer sget Master | awk -vORS=' ' '/Mono:/ {print($6$4)}' );
-
-# Network
-NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/^\* //')
-
-# Date and Time
-CLOCK=$( date '+%H:%M' );
-
-# Overall output command
-#DWM_STATUS="$NETPROFILE | [$POWERSOURCE][$BATTERYLEVEL] | $VOLUME| $CLOCK";
-DWM_STATUS="$NETPROFILE | [$POWERSOURCE][$BATTERYLEVEL] | $CLOCK";
-xsetroot -name "$DWM_STATUS";
-sleep $DWM_REFRESH_INT;
-
+	# Power Status
+	if [[ "$( cat /sys/class/power_supply/AC/online )" -eq "1" || "$( cat /sys/class/power_supply/ACAD/online )" -eq "1" ]]; then
+        	POWERSOURCE="+";
+	else
+        	POWERSOURCE="-";
+	fi;
+	
+	# Battery Status
+	BATTERYLEVEL=$(( `cat /sys/class/power_supply/BAT0/charge_now` * 100 / `cat /sys/class/power_supply/BAT0/charge_full` ));
+	if [ -d /sys/class/power_supply/BAT1 ]; then
+		SPAREBATTERYLEVEL=$(( `cat /sys/class/power_supply/BAT1/charge_now` * 100 / `cat /sys/class/power_supply/BAT1/charge_full` ));
+	fi;
+	
+	# Network Status
+	NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/^\* //');
+	
+	# Date and Time
+	CLOCK=$( date '+%H:%M' );
+	
+	# Overall output command
+	DWM_STATUS="$NETPROFILE | [$SPAREBATTERYLEVEL][$POWERSOURCE][$BATTERYLEVEL] | $CLOCK";
+	xsetroot -name "$DWM_STATUS";
+	sleep $DWM_REFRESH_INT;
+	
 done &
