764ba71dea12dd6cd776e69078840459b70f9320
Link Satonaka <link.satonaka@outlook.com>
2015-09-04 01:34:25 -0400

    moving away from mmkeyosd, first attempt at using sxhkd and twmn

--- home/Link/.bin/brightup.sh
+++ home/Link/.bin/brightup.sh
@@ -2,5 +2,6 @@
 xbacklight -inc 10
 BRFILE=/sys/class/backlight/acpi_video0/brightness
 BRMAXFILE=/sys/class/backlight/acpi_video0/max_brightness
-echo `cat $BRFILE` `cat $BRMAXFILE` | awk 'END{print int($1/$2*100)}'
+bright=$(echo `cat $BRFILE` `cat $BRMAXFILE` | awk 'END{print int($1/$2*100)}')
+twmnc -c "Brightness: $bright" --id 99
 
---
+++ home/Link/.bin/vol_lower.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+#The math here requires zsh, this will not run in bash or sh
+#Todo: make this more portable
+
+vol=$(amixer set Master 5%- | sed -n -e 's/.*Playback.*\[\([0-9]*\)%\].*/\1/p' | head -n 1)
+#bar=$(echo -ne [ ; printf '%.0s#' {1..$vol}; printf '%.0s ' {1..$((100-$vol))}; echo ])
+twmnc -c "Volume: $vol" --id 99
