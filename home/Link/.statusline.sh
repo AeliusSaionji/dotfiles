02b1f96cf4db9b2f89a8af84f4d26d548368e572
Link Satonaka <link.satonaka@outlook.com>
2015-09-05 05:12:25 -0400

    using statnot, added load avg

---
+++ home/Link/.statusline.sh
@@ -0,0 +1,28 @@
+if [ $# -eq 0 ]; then
+	# Power Status
+	if [ "$(cat /sys/class/power_supply/AC/online)" -eq "1" ]; then
+        	POWERSOURCE="+";
+	else
+        	POWERSOURCE="-";
+	fi;
+	
+	# Battery Status
+	BATTERYLEVEL=$(cat /sys/class/power_supply/BAT0/capacity);
+	if [ -d /sys/class/power_supply/BAT1 ]; then
+        	SPAREBATTERYLEVEL=$(cat /sys/class/power_supply/BAT1/capacity);
+	fi;
+	
+	# Network Status
+	NETPROFILE=$(netctl list | sed -e '/\*/!d' -e 's/^\* //');
+
+	# Date and Time
+	CLOCK=$( date '+%H:%M' );
+
+	# Load
+	LOAD="`cat /proc/loadavg | awk '{print $1, $2, $3}'`";
+	
+	# Overall output command
+	echo "$LOAD | $NETPROFILE | [$SPAREBATTERYLEVEL][$POWERSOURCE][$BATTERYLEVEL] | $CLOCK";
+else
+    echo "NOTIFICATION: $1";
+fi
