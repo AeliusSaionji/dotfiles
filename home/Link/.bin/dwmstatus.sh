642e8ed27976beb21168f63d39e0671a3983338c
Link Satonaka <link.satonaka@outlook.com>
2015-09-07 11:24:49 -0400

    formatting

--- home/Link/.bin/dwmstatus.sh
+++ home/Link/.bin/dwmstatus.sh
@@ -8,13 +8,13 @@ else
 fi;
 
 # Battery Status
-BATTERYLEVEL=[$(cat /sys/class/power_supply/BAT0/capacity)];
+BATTERYLEVEL=$(cat /sys/class/power_supply/BAT0/capacity);
 if [ -d /sys/class/power_supply/BAT1 ]; then
-	SPAREBATTERYLEVEL=[$(cat /sys/class/power_supply/BAT1/capacity)];
+	SPAREBATTERYLEVEL=$(cat /sys/class/power_supply/BAT1/capacity);
 fi;
 
 # Network Status
-NETPROFILE="$(netctl list | sed -e '/\*/!d' -e 's/^\* //') |";
+NETPROFILE="$(netctl list | sed -e '/\*/!d' -e 's/^\* //')";
 
 # Date and Time
 CLOCK=$( date '+%H:%M' );
@@ -23,4 +23,4 @@ CLOCK=$( date '+%H:%M' );
 LOAD="`cat /proc/loadavg | awk '{print $1, $2, $3}'`";
 
 # Overall output command
-xsetroot -name "$LOAD | $NETPROFILE $SPAREBATTERYLEVEL[$POWERSOURCE]$BATTERYLEVEL | $CLOCK";
+xsetroot -name "$LOAD | $NETPROFILE | $SPAREBATTERYLEVEL [$POWERSOURCE] $BATTERYLEVEL | $CLOCK";
