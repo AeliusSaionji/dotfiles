c1951ca9d2a3f7ef18bac3dddc3a766f64b90544
Link Satonaka <link.satonaka@outlook.com>
2015-09-09 19:21:53 -0400

    ovedrhauled automated setup

--- create-links.sh
+++ create-links.sh
@@ -1,7 +1,33 @@
 #!/bin/zsh
-# (*|.*) needs bash or zsh
-ln -s $HOME/dotfiles/home/Link/(*|.*) $HOME/
-host="$(hostnamectl status --static)"
-if [ -d $HOME/dotfiles/$host ]; then
-	ln -s $HOME/dotfiles/$host/home/Link/(*|.*) $HOME/
+if [ -d ../dotfiles ]; then
+	# (*|.*) needs bash or zsh
+	ln -s home/Link/(*|.*) $HOME/
+	host="$(hostnamectl status --static)"
+	cwd="$(pwd)"
+	if [ -d ./$host ]; then
+		ln -s $cwd/$host/home/Link/(*|.*) $HOME/
+		ls -l $cwd/$host/etc/modprobe.d
+		printf "symlink /etc/modprobe.d (yes/no)?"
+		read proceed
+		if [ "$proceed" = "yes" ]; then
+			sudo ln -s $cwd/$host/etc/modprobe.d/* /etc/modprobe.d/
+		fi
+		proceed=""
+		ls -l $cwd/$host/etc/sysctl.d
+		printf "symlink /etc/sysctl.d (yes/no)?"
+		read proceed
+		if [ "$proceed" = "yes" ]; then
+			sudo ln -s $cwd/$host/etc/sysctl.d/* /etc/sysctl.d/
+		fi
+		proceed=""
+		ls -l $cwd/$host/etc/X11/xorg.conf.d
+		printf "symlink /etc/X11/xorg.conf.d (yes/no)?"
+		read proceed
+		if [ "$proceed" = "yes" ]; then
+			sudo ln -s $cwd/$host/etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
+		fi
+	fi
+else
+	printf "Run this script in the root of the dotfiles folder"
+	exit 1
 fi
