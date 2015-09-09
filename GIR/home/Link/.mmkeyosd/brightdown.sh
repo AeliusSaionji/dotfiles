6a5fc46486d7cbfc906c1c3de76e167984179db4
Link Satonaka <link.satonaka@outlook.com>
2015-09-09 19:23:41 -0400

    no longer using mmkeyosd

--- GIR/home/Link/.mmkeyosd/brightdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xbacklight -dec 10
-BRFILE=/sys/class/backlight/acpi_video0/brightness
-BRMAXFILE=/sys/class/backlight/acpi_video0/max_brightness
-echo `cat $BRFILE` `cat $BRMAXFILE` | awk 'END{print int($1/$2*100)}'
-
