8f63a3a0076725681f22ff161ffc20d5bbe79470
Link Satonaka <link.satonaka@outlook.com>
2015-09-09 20:07:47 -0400

    remove mmkeyosd

--- Saya/home/Link/.mmkeyosd/brightdown.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xbacklight -dec 10
-BRFILE=/sys/class/backlight/intel_backlight/brightness
-BRMAXFILE=/sys/class/backlight/intel_backlight/max_brightness
-echo `cat $BRFILE` `cat $BRMAXFILE` | awk 'END{print int($1/$2*100)}'
-
