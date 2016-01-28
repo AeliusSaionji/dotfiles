17e879ede3b5c99a82d5b343c8f203d84dbfe671
Link <link.satonaka@outlook.com>
2016-01-28 01:29:48 -0500

    dirty dirty dirty

---
+++ home/Link/.bin/trans-exempt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+dasWin=$(xprop | sed -e '/^WM_CLASS/!d' -e 's/^.*\,\s//' -e 's/\"/'\''/g')
+if grep $dasWin /tmp/compton.conf ; then
+	sed /tmp/compton.conf -e '/'$dasWin'/d' > /tmp/compt
+else
+	sed /tmp/compton.conf -e '/mpv/i\\t"class_g = '$dasWin'",' > /tmp/compt
+fi
+pkill compton
+cp /tmp/compt /tmp/compton.conf
+compton -b --config /tmp/compton.conf
