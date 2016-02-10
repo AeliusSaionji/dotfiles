a9026f4c25b729b95ba766f0878bd8e4e6e14e54
Link <link.satonaka@outlook.com>
2016-02-10 00:57:39 -0500

    remove irssi config

--- home/Link/.irssi/synclogs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-timestamp=$(date +%Y-%m)
-mkdir -p ~/irclogs/Freenode
-echo "/set autolog off" > ~/.irssi/remote-control
-/usr/bin/rsync -am --no-o --no-g --no-super --delete rsync://25.18.109.202/irclogs/Freenode/*-${timestamp}.log ~/irclogs/Freenode/ && printf "logs synced\n" && echo "/set autolog on" > ~/.irssi/remote-control
