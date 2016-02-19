6c2c8a8f1faa8746ff4b4246aadb05e410134111
Link <link.satonaka@outlook.com>
2016-02-19 08:18:28 -0500

    no mouse grabbing, directly point to drive

--- home/Link/.bin/qemu.sh
+++ home/Link/.bin/qemu.sh
@@ -3,7 +3,8 @@
 # Create a scheduled task in the Windows guest to run the following at boot:
 # ssh 10.0.2.2 -R 3389:localhost:3389
 # RDP localhost, profit.
+
 qemu-system-x86_64 \
--drive file=~/WinX.img,index=0,media=disk,if=virtio,format=raw \
+-drive file="/home/Link/WinX.img",index=0,media=disk,if=virtio,format=raw \
 -cpu host -smp cores=2 -m 4096 -machine type=pc,accel=kvm -enable-kvm \
--nographic -soundhw hda &
+-nographic -soundhw hda -usbdevice tablet &
