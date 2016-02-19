819e502fca4081394a846135ab5b111b0b292aa3
Link <link.satonaka@outlook.com>
2016-02-18 23:12:39 -0500

    implement wpa

--- home/Link/.bin/powerfondler.sh
+++ home/Link/.bin/powerfondler.sh
@@ -8,16 +8,27 @@ for pid in $pids; do
                 /proc/$pid/environ | sed -e 's/DBUS_SESSION_BUS_ADDRESS=//'`
 done
 
+# udev 99-lowbat.rules
 case "$1" in
 	"low")
-	        DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+		DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
 			notify-send -u normal "Low Battery" ;;
 	"critical")
-	        DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+		DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
 			notify-send -u critical "Low Battery" "Find power soon!" ;;
 	"sleep")
-	        DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+		DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
 			notify-send -u critical "Suspend Imminent" "The system is going down in under two minutes!" ;;
 	"hibernate")
 		echo "not implemented" ;;
+esac
+
+# wpa_cli -a powerfondler.sh
+case "$2" in
+	"CONNECTED")
+		DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+			notify-send "WiFi" "connection established" ;;
+	"DISCONNECTED")
+		DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+			notify-send "WiFi" "connection lost" ;;
 esac
