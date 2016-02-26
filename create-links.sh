868e297fd7db31f3e9b3a8481793215a67ed2717
Link <link.satonaka@outlook.com>
2016-02-25 20:49:01 -0500

    overwrite certain links, include general etc

--- create-links.sh
+++ create-links.sh
@@ -2,38 +2,56 @@
 color='\033[1;35m'
 NC='\033[0m' # No Color
 if [ -d ../dotfiles ]; then
+	printf "\nChange etc ownership to root "
+	sudo chown -R root $PWD/etc
+	sudo chgrp -R root $PWD/etc
+	printf "\n\n----------> ls /etc/systemd/system\n"
+	ls -l $PWD/etc/systemd/system
+	printf "\n${color}symlink /etc/systemd/system (yes/no)?>${NC} "
+	read proceed
+	if [ "$proceed" = "yes" ]; then
+		sudo ln -sf $PWD/etc/systemd/system/* /etc/systemd/system/
+	fi
+	proceed=""
+	printf "\n\n----------> ls /etc/udev/rules.d\n"
+	ls -l $PWD/etc/udev/rules.d
+	printf "\n${color}symlink /etc/udev/rules.d (yes/no)?>${NC} "
+	read proceed
+	if [ "$proceed" = "yes" ]; then
+		sudo ln -sf $PWD/etc/udev/rules.d/* /etc/udev/rules.d/
+	fi
+	proceed=""
 	host="$(hostnamectl status --static)"
-	cwd="$(pwd)"
 	# (*|.*) needs bash or zsh
-	ln -s $cwd/home/Link/(*|.*) $HOME/
-	if [ -d ./$host ]; then
-		ln -s $cwd/$host/home/Link/(*|.*) $HOME/
-		if [ -d ./$host/etc ]; then
+	ln -s $PWD/home/Link/(*|.*) $HOME/
+	if [ -d $PWD/$host ]; then
+		ln -s $PWD/$host/home/Link/(*|.*) $HOME/
+		if [ -d $PWD/$host/etc ]; then
 			printf "\nChange etc ownership to root "
-			sudo chown -R root ./$host/etc
-			sudo chgrp -R root ./$host/etc
+			sudo chown -R root $PWD/$host/etc
+			sudo chgrp -R root $PWD/$host/etc
 			printf "\n\n----------> ls /etc/modprobe.d\n"
-			ls -l $cwd/$host/etc/modprobe.d
+			ls -l $PWD/$host/etc/modprobe.d
 			printf "\n${color}symlink /etc/modprobe.d (yes/no)?>${NC} "
 			read proceed
 			if [ "$proceed" = "yes" ]; then
-				sudo ln -s $cwd/$host/etc/modprobe.d/* /etc/modprobe.d/
+				sudo ln -sf $PWD/$host/etc/modprobe.d/* /etc/modprobe.d/
 			fi
 			proceed=""
 			printf "\n\n----------> ls /etc/sysctl.d\n"
-			ls -l $cwd/$host/etc/sysctl.d
+			ls -l $PWD/$host/etc/sysctl.d
 			printf "\n${color}symlink /etc/sysctl.d (yes/no)?>${NC} "
 			read proceed
 			if [ "$proceed" = "yes" ]; then
-				sudo ln -s $cwd/$host/etc/sysctl.d/* /etc/sysctl.d/
+				sudo ln -sf $PWD/$host/etc/sysctl.d/* /etc/sysctl.d/
 			fi
 			proceed=""
 			printf "\n\n----------> ls /etc/X11/xorg.conf.d\n"
-			ls -l $cwd/$host/etc/X11/xorg.conf.d
+			ls -l $PWD/$host/etc/X11/xorg.conf.d
 			printf "\n${color}symlink /etc/X11/xorg.conf.d (yes/no)?>${NC} "
 			read proceed
 			if [ "$proceed" = "yes" ]; then
-				sudo ln -s $cwd/$host/etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
+				sudo ln -sf $PWD/$host/etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
 			fi
 		fi
 	fi
