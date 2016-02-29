da942fa87762a7888b484a85069197d84d3604b4
Link <link.satonaka@outlook.com>
2016-02-29 03:14:08 -0500

    popup

--- home/Link/.bin/trans-exempt.sh
+++ home/Link/.bin/trans-exempt.sh
@@ -7,8 +7,10 @@ activeWin=$(xprop -root _NET_ACTIVE_WINDOW | sed -e 's/^.*id\s#\s//')
 dasWin=$(xprop -id $activeWin | sed -e '/^WM_CLASS/!d' -e 's/^.*\,\s//' -e 's/\"/'\''/g')
 if grep $dasWin /tmp/compton.conf ; then
 	sed /tmp/compton.conf -e '/'$dasWin'/d' > /tmp/compt
+	notify-send "Transparency Disabled" "$dasWin"
 else
 	sed /tmp/compton.conf -e '/class_g = '\''mpv'\''/i\\t"class_g = '$dasWin'",' > /tmp/compt
+	notify-send "Transparency Enabled" "$dasWin"
 fi
 pkill compton
 cp /tmp/compt /tmp/compton.conf
