1eb005a7cf0c605ae8a59aec701dc3b919f56c3c
Link <link.satonaka@outlook.com>
2016-06-05 02:04:06 -0400

    what the hell, qemu alternates between needing this, and refusing to work with this

--- home/Link/.bin/qemu.sh
+++ home/Link/.bin/qemu.sh
@@ -2,6 +2,7 @@
 # Create a scheduled task in the Windows guest to run the following at boot:
 # ssh 10.0.2.2 -R 3389:localhost:3389
 # RDP localhost, profit.
+#!/bin/bash
 
 qemu-system-x86_64 \
 -bios /usr/share/ovmf/ovmf_x64.bin  -net nic,model=virtio -net user \
