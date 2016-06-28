e35825e8a9d1847bd08379ecaf095686dd6385bd
Link <link.satonaka@outlook.com>
2016-06-27 23:28:08 -0400

    Revert "no longer used"
    This reverts commit 1fb7c1ed45f1ef0d2c420987bc54e9d748a14f15.

--- home/Link/.bin/dctl.sh
+++ home/Link/.bin/dctl.sh
@@ -1,31 +1,15 @@
 #!/bin/sh
 
 #TODO
-# - implement status/enable/etc, pipe output to dmenu
-# - option to ask for services or action first
-#	asking for service first allows us to print its status seamlessly
+# - implement status/other, pipe output to dmenu
+# - elevate with sudo askpass + enable --system
 # - find a way to indicate failed/running state within the initial list.
-# - libnotify?
+#	might require parsing output of systemctl,
+#	printing to dmenu like: unit.service -- FAILED
+#	re-parsing what dmenu returns
 
-export SUDO_ASKPASS=~/.bin/askpass.sh
-font="inconsolata:size=8"
-action=$(echo -e 'start\nstop\nrestart\ndaemon reload\n--user start\n--user stop\n--user restart\n--user daemon reload' | dmenu -fn $font)
+action=$(echo -e 'start\nstop\nrestart' | dmenu -fn inconsolata:size=8)
+# List user units
+unit=$(ls .config/systemd/user/ --ignore=\*.wants | dmenu -l 10 -fn inconsolata:size=8)
 
-# if action is status
-#if [ "$action" = '--user status' -o "$action" = 'status' ]; then
-	# combine output of both --user and --system
-# if action contains '--user'
-if [ -z $(echo $action | sed -e '/--user/d') ]; then
-	# List --user units
-	unit=$(systemctl --user list-unit-files --no-pager \
-		|  grep -P 'enabled|enabled-runtime|linked|linked-runtime|masked|masked-runtime|static|indirect|disabled|generated|transient|bad' \
-		| dmenu -fn $font -l 20 | grep -o -P '^.*\.\w+')
-	systemctl $action $unit
-else # action for system
-	# List units
-	unit=$(systemctl list-unit-files --no-pager \
-		|  grep -P 'enabled|enabled-runtime|linked|linked-runtime|masked|masked-runtime|static|indirect|disabled|generated|transient|bad' \
-		| dmenu -fn $font -l 20 | grep -o -P '^.*\.\w+')
-	sudo -A systemctl $action $unit
-fi
-
+systemctl --user $action $unit
