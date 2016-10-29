7f174c8ced6e54b230eec440a96cdca64d037e88
Link <link.satonaka@outlook.com>
2016-10-29 00:53:11 -0400

    better sed hook, added comments

--- common/home/user/.bin/trans-exempt.sh
+++ common/home/user/.bin/trans-exempt.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
-# TODO 
-# - make insertion generic, don't hook around mpv rule
 
+# The runtime temporary config
 volatileConfig="$HOME/.cache/compton.conf"
 
+# Ensure cache config exists via compton.service ExecStartPre
 case $1 in
 	createcache)
 		[ -e $volatileConfig ] || cp $HOME/.config/compton.conf $volatileConfig
 		exit ;;
 esac
 
+# Retrieve class name of active window
 activeWin=$(xprop -root _NET_ACTIVE_WINDOW | sed -e 's/^.*id\s#\s//')
 dasWin=$(xprop -id $activeWin | sed -e '/^WM_CLASS/!d' -e 's/^.*\,\s//' -e 's/\"/'\''/g')
 
+# Remove active window from config exclude list
 if grep $dasWin $volatileConfig ; then
 	sed -i $volatileConfig -e '/'$dasWin'/d'
 	notifymsg="Transparency Enabled"
+# Add active window to config exclude list
 else
-	sed -i $volatileConfig -e '/class_g = '\''mpv'\''/i\\t"class_g = '$dasWin'",'
+	sed -i $volatileConfig -e 's/focus-exclude = \[/focus-exclude = [\n\t"class_g = '$dasWin'",/'
 	notifymsg="Transparency Disabled"
 fi
+
+# Restart compton to apply changes
 systemctl --user restart compton.service
 sleep 1
 notify-send "$notifymsg" "$dasWin"
