01d26cb4f7dfd13111bf6302c87049c8bb210738
Link <link.satonaka@outlook.com>
2016-10-29 00:31:02 -0400

    Use sed properly. Use more variables. Implement startup sequence for ExecStartPre

--- common/home/user/.bin/trans-exempt.sh
+++ common/home/user/.bin/trans-exempt.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 # TODO 
-# - figure out why using > redirect on compton.conf just produces an empty file
 # - make insertion generic, don't hook around mpv rule
+
+volatileConfig="$HOME/.cache/compton.conf"
+
+case $1 in
+	createcache)
+		[ -e $volatileConfig ] || cp $HOME/.config/compton.conf $volatileConfig
+		exit ;;
+esac
 
 activeWin=$(xprop -root _NET_ACTIVE_WINDOW | sed -e 's/^.*id\s#\s//')
 dasWin=$(xprop -id $activeWin | sed -e '/^WM_CLASS/!d' -e 's/^.*\,\s//' -e 's/\"/'\''/g')
-if grep $dasWin /tmp/compton.conf ; then
-	sed /tmp/compton.conf -e '/'$dasWin'/d' > /tmp/compt
+
+if grep $dasWin $volatileConfig ; then
+	sed -i $volatileConfig -e '/'$dasWin'/d'
 	notifymsg="Transparency Enabled"
 else
-	sed /tmp/compton.conf -e '/class_g = '\''mpv'\''/i\\t"class_g = '$dasWin'",' > /tmp/compt
+	sed -i $volatileConfig -e '/class_g = '\''mpv'\''/i\\t"class_g = '$dasWin'",'
 	notifymsg="Transparency Disabled"
 fi
-cp /tmp/compt /tmp/compton.conf
 systemctl --user restart compton.service
 sleep 1
 notify-send "$notifymsg" "$dasWin"
