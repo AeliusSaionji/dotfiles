38d26c581759c56de17b34f721c37619ecf58e20
link <link.satonaka@outlook.com>
2017-10-05 22:31:21 -0400

    add passmenu + search engines

--- common/home/user/.config/qutebrowser/config.py
+++ common/home/user/.config/qutebrowser/config.py
@@ -15,11 +15,14 @@ c.url.searchengines['a'] = 'https://smile.amazon.com/s/url=search-alias%3Daps&fi
 c.url.searchengines['aur'] = 'https://aur.archlinux.org/packages/?O=0&K={}'
 c.url.searchengines['aw'] = 'https://wiki.archlinux.org/index.php?search={}'
 c.url.searchengines['g'] = 'https://www.google.com/search?q={}'
+c.url.searchengines['pc'] = 'https://pcgamingwiki.com/w/index.php?search={}'
+c.url.searchengines['y'] = 'https://www.youtube.com/results?search_query={}'
 c.url.searchengines['w'] = 'https://en.wikipedia.org/w/index.php?search={}'
 c.url.start_pages = ["about:blank"]
 
 # Bindings
 config.bind('<F3>', 'spawn --detach mpv {url}', mode='normal')
+config.bind('pf', 'spawn --detach passmenu', mode='normal')
 config.bind('O', 'set-cmd-text :open {url:pretty}', mode='normal', force=True)
 config.bind('t', 'set-cmd-text -s :open -t', mode='normal', force=True)
 config.bind('T', 'set-cmd-text :open -t {url:pretty}', mode='normal', force=True)
