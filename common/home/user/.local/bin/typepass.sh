3967e1cdfec4edd81c06bde5ab3837f479c7103b
Link <link.satonaka@outlook.com>
2018-06-30 20:44:39 -0400

    simple script to type logins

---
+++ common/home/user/.local/bin/typepass.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Requires: find sed dmenu xdotool
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+
+# Generate list of password files, select one via dmenu
+site=$( 
+	find $prefix -path ./.git -prune -o -type f -iname '*.gpg' -printf '%P\n' \
+	| sed 's/\.gpg$//' \
+	| dmenu "$@"
+)
+
+[ -n $password ] || exit
+
+# Password is first line of file
+password=$(pass show "$site" | sed -n 1p)
+# Username is name of file
+username=$(find $prefix -wholename "*$site*" -printf '%f' | sed 's/\.gpg$//')
+# Simpler code, but adds dependency on awk
+#username=$(printf '%s' "$site" | awk -F / '{print $2}')
+pstr=$(printf '%s\t%s' "$username" "$password")
+echo $pstr
+#xdotool type "$pstr"
