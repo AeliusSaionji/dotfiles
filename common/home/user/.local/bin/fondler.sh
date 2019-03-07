4e67c5dade4ac17de927c9e563b2502055590a9c
aelius <aeliussaionji@outlook.com>
2019-03-07 03:39:28 -0500

    remove rotation from fondler

--- common/home/user/.local/bin/fondler.sh
+++ common/home/user/.local/bin/fondler.sh
@@ -17,34 +17,12 @@ case "$1" in
 		xsel -co | xargs -r xdg-open ;;
 	"lock")
 		xset s activate && sleep 2 && xset dpms force off ;;
-	"rotate")
-		deviceName=$(xsetwacom --list devices | sed -e '/touch/!d' -e 's/\s\w\+\s\+id.*$//')
-		if [ -z $deviceName ]; then
-			notify-send "Touch device not found" "you're probably using libinput"
-		fi
-		# Needs to be tested in a multi monitor setup
-		displayName=$(xrandr | sed -e '2!d' -e 's/\sconnected.*$//')
-		orientation=$(xrandr | sed -e '/\sconnected/!d' -e 's/\w\+\sconnected\s[0-9]\+x[0-9]\++[0-9]\++[0-9]\+\s//' -e 's/(normal.*$//' -e '/^$/d')
-		if [ -z $orientation ]; then
-			xrandr --output $displayName --rotate right
-			xsetwacom --set "$deviceName stylus" Rotate cw
-			xsetwacom --set "$deviceName eraser" Rotate cw
-			xsetwacom --set "$deviceName touch"  Rotate cw
-			echo right
-		else
-			xrandr --output $displayName --rotate normal
-			xsetwacom --set "$deviceName stylus" Rotate none
-			xsetwacom --set "$deviceName eraser" Rotate none
-			xsetwacom --set "$deviceName touch"  Rotate none
-			echo normal
-		fi ;;
 	"voldown")
 		vol=$(amixer set Master 5%- | sed -n -e 's/.*Playback.*\[\([0-9]*\)%\].*/\1/p' | head -n 1)
 		notify-send -u low -t 1000 -h int:value:$vol Volume fondler ;;
 	"volup")
 		vol=$(amixer set Master 5%+ | sed -n -e 's/.*Playback.*\[\([0-9]*\)%\].*/\1/p' | head -n 1)
 		notify-send -u low -t 1000 -h int:value:$vol Volume fondler ;;
-
 	"j4") # j4dmenu arguments
 		j4-dmenu-desktop --dmenu="dmenu -m \"$2\" -i" \
 			--usage-log=${HOME}/.cache/j4-dmenu-desktop-cache --term="st" ;;
