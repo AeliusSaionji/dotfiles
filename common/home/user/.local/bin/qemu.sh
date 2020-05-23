0941ebbdf9b66ab18f929411807811668c772474
aelius <aeliussaionji@outlook.com>
2020-05-23 11:50:52 -0400

    qemu script updates

--- common/home/user/.local/bin/qemu.sh
+++ common/home/user/.local/bin/qemu.sh
@@ -1,11 +1,7 @@
-# Default networking settings
-# Create a scheduled task in the Windows guest to run the following at boot:
-# ssh 10.0.2.2 -R 3389:localhost:3389
-# RDP localhost, profit.
-#!/bin/bash
+#!/bin/sh
 
 qemu-system-x86_64 \
--bios /usr/share/ovmf/ovmf_x64.bin  -net nic,model=virtio -net user \
--cpu host -smp cores=2 -m 4096 -machine type=pc,accel=kvm -enable-kvm \
--drive file="$HOME/documents/qemu/WinX.img",index=0,media=disk,if=virtio,format=raw,cache=none \
--soundhw hda -usbdevice tablet -nographic
+ -enable-kvm -cpu host -smp cores=4 -m 4G \
+ -nic user,hostfwd=tcp::33890-:3389,hostfwd=udp::33890-:3389 \
+ -drive file="$HOME/.local/qemu/WinX.qcow2" \
+ -nographic
