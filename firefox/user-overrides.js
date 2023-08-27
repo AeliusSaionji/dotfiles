671871d1e4f41d9ba1eccb437568e8fc690ba3f3
aelius <aelius@>
2023-08-27 19:27:30 -0400

    firefox arkenfox + general prefs

---
+++ firefox/user-overrides.js
@@ -0,0 +1,100 @@
+/* override recipe: enable session restore ***/
+user_pref("browser.startup.page", 3); // 0102
+user_pref("privacy.clearOnShutdown.history", false); // 2811
+user_pref("privacy.cpd.history", false); // 2820 optional to match when you use Ctrl-Shift-Del
+
+user_pref("webgl.disabled", false); //4520
+user_pref("signon.rememberSignons", false); //5003
+user_pref("browser.taskbar.previews.enable", false); //5015
+user_pref("extensions.formautofill.addresses.enabled", false); // 5017
+user_pref("extensions.formautofill.creditCards.enabled", false); // 5017
+user_pref("extensions.formautofill.heuristics.enabled", false); // 5017
+
+
+/* 1003: disable storing extra session data [SETUP-CHROME]
+ * define on which sites to save extra session data such as form content, cookies and POST data
+ * 0=everywhere, 1=unencrypted sites, 2=nowhere ***/
+user_pref("browser.sessionstore.privacy_level", 0);
+/* 1005: disable automatic Firefox start and session restore after reboot [FF62+] [WINDOWS]
+ * [1] https://bugzilla.mozilla.org/603903 ***/
+user_pref("toolkit.winRegisterApplicationRestart", true);
+/* 1006: disable favicons in shortcuts
+ * URL shortcuts use a cached randomly named .ico file which is stored in your
+ * profile/shortcutCache directory. The .ico remains after the shortcut is deleted
+ * If set to false then the shortcuts use a generic Firefox icon ***/
+user_pref("browser.shell.shortcutFavicons", true);
+
+/* 2651: enable user interaction for security by always asking where to download
+ * [SETUP-CHROME] On Android this blocks longtapping and saving images
+ * [SETTING] General>Downloads>Always ask you where to save files ***/
+user_pref("browser.download.useDownloadDir", true);
+/* 2652: disable downloads panel opening on every download [FF96+] ***/
+user_pref("browser.download.alwaysOpenPanel", true);
+
+
+//aelius
+user_pref("extensions.pocket.enabled", false);
+user_pref("config.trim_on_minimize", true); //Windows trim memory when fx minimized
+user_pref("network.http.windows-sso.enabled", true);
+user_pref("reader.color_scheme", "sepia");
+user_pref("reader.content_width", 5);
+user_pref("reader.font_size", 3);
+user_pref("reader.font_type", "serif");
+user_pref("reader.line_height", 2);
+user_pref("reader.parse-on-load.force-enabled", true); //readability always available
+user_pref("font.name.sans-serif.x-western", "Sylfaen"); //serif font for sans serif
+//user_pref("font.name.serif.x-western", "Sitka Display");
+user_pref("browser.display.use_document_fonts", 0); //override web page fonts
+user_pref("devtools.editor.keymap", "vim"); //binds for source editor
+user_pref("browser.download.start_downloads_in_tmp_dir", true); //for play-with m3u spam
+
+///  NATURAL SMOOTH SCROLLING V4 "SHARP" - AveYo, 2020-2022             preset     [default]
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS",   12);//NSS    [120]
+user_pref("general.smoothScroll.msdPhysics.enabled",                    true);//NSS  [false]
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant",   200);//NSS   [1250]
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant",       250);//NSS   [1000]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS",           25);//NSS     [12]
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio",     "2.0");//NSS    [1.3]
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant",      250);//NSS   [2000]
+user_pref("general.smoothScroll.currentVelocityWeighting",             "1.0");//NSS ["0.25"]
+user_pref("general.smoothScroll.stopDecelerationWeighting",            "1.0");//NSS  ["0.4"]
+
+/// adjust multiply factor for mousewheel - or set to false if scrolling is way too fast  
+user_pref("mousewheel.system_scroll_override.horizontal.factor",         200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override.vertical.factor",           200);//NSS    [200]
+user_pref("mousewheel.system_scroll_override_on_root_content.enabled",  true);//NSS   [true]
+user_pref("mousewheel.system_scroll_override.enabled",                  true);//NSS   [true]
+
+/// adjust pixels at a time count for mousewheel - cant do more than a page at once if <100
+user_pref("mousewheel.default.delta_multiplier_x",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_y",                       100);//NSS    [100]
+user_pref("mousewheel.default.delta_multiplier_z",                       100);//NSS    [100]
+
+///  this preset will reset couple extra variables for consistency
+user_pref("apz.allow_zooming",                                          true);//NSS   [true]
+user_pref("apz.force_disable_desktop_zooming_scrollbars",              false);//NSS  [false]
+user_pref("apz.paint_skipping.enabled",                                 true);//NSS   [true]
+user_pref("apz.windows.use_direct_manipulation",                        true);//NSS   [true]
+user_pref("dom.event.wheel-deltaMode-lines.always-disabled",           false);//NSS  [false]
+user_pref("general.smoothScroll.durationToIntervalRatio",                200);//NSS    [200]
+user_pref("general.smoothScroll.lines.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.lines.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.other.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMaxMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pages.durationMinMS",                    150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMaxMS",                   150);//NSS    [150]
+user_pref("general.smoothScroll.pixels.durationMinMS",                   150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMaxMS",               150);//NSS    [150]
+user_pref("general.smoothScroll.scrollbars.durationMinMS",               150);//NSS    [150]
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS",               200);//NSS    [200]
+user_pref("general.smoothScroll.mouseWheel.durationMinMS",                50);//NSS     [50]
+user_pref("layers.async-pan-zoom.enabled",                              true);//NSS   [true]
+user_pref("layout.css.scroll-behavior.spring-constant",                "250");//NSS    [250]
+user_pref("mousewheel.transaction.timeout",                             1500);//NSS   [1500]
+user_pref("mousewheel.acceleration.factor",                               10);//NSS     [10]
+user_pref("mousewheel.acceleration.start",                                -1);//NSS     [-1]
+user_pref("mousewheel.min_line_scroll_amount",                             5);//NSS      [5]
+user_pref("toolkit.scrollbox.horizontalScrollDistance",                    5);//NSS      [5]
+user_pref("toolkit.scrollbox.verticalScrollDistance",                      3);//NSS      [3]
+/// source: https://github.com/AveYo/fox/blob/main/Natural%20Smooth%20Scrolling%20for%20user.js
