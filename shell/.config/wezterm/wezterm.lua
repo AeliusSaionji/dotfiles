13a944d159b5e53c4556015d8cae62268c6f3a66
aelius <aelius@>
2024-02-08 02:56:45 -0500

    wezterm tinkering

--- shell/.config/wezterm/wezterm.lua
+++ shell/.config/wezterm/wezterm.lua
@@ -20,11 +20,15 @@ config.animation_fps = 120
 config.visual_bell = {
   fade_in_duration_ms = 750,
   fade_out_duration_ms = 75,
-  target = "CursorColor",
-}
+  target = "CursorColor" }
 -- config.colors = {
 --   visual_bell = '#FFFFFF',
 -- }
+
+config.window_background_gradient = {
+  colors = { '#0f0c29', '#302b63', '#24243e' },
+  orientation = { Radial = { cx = 0.75, cy = 0.75, radius = 1.25 } } }
+
 config.enable_kitty_keyboard = true
 
 config.font = wezterm.font 'VictorMono Nerd Font'
@@ -32,24 +36,14 @@ config.font = wezterm.font 'VictorMono Nerd Font'
 config.harfbuzz_features = { 'calt', 'clig', 'liga', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08' }
 
 config.font_size = 12
-config.line_height = 1.0
+config.line_height = 1.2
 config.adjust_window_size_when_changing_font_size = false
--- config.color_scheme = 'Laserwave (Gogh)'
 -- config.use_fancy_tab_bar = true
 config.enable_tab_bar = false
 config.switch_to_last_active_tab_when_closing_tab = true
 config.enable_scroll_bar = true
--- config.quit_when_all_windows_are_closed = false
 config.scrollback_lines = 5000
 config.ssh_backend = "Ssh2"
 config.default_prog = { 'pwsh.exe', '-NoLogo' }
-
-wezterm.on('update-status', function(window, pane)
-  local meta = pane:get_metadata() or {}
-  if meta.is_tardy then
-    local secs = meta.since_last_response_ms / 1000.0
-    window:set_right_status(string.format('tardy: %5.1fs⏳', secs))
-  end
-end)
 
 return config
