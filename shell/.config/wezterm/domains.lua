655cffdcb8701a1fb1318e5637b2f01f3498a13d
aelius <aelius@>
2024-07-14 19:52:04 -0400

    wezterm: process ssh hosts from ssh config

--- shell/.config/wezterm/domains.lua
+++ shell/.config/wezterm/domains.lua
@@ -1,24 +1,21 @@
+local wezterm = require 'wezterm'
+local ssh_domains = {}
+
+for host, config in pairs(wezterm.enumerate_ssh_hosts()) do
+  table.insert(ssh_domains, {
+    -- the name can be anything you want; we're just using the hostname
+    name = host,
+    -- remote_address must be set to `host` for the ssh config to apply to it
+    remote_address = host,
+    assume_shell = 'Posix',
+    multiplexing = "WezTerm",
+    local_echo_threshold_ms = 10,
+  })
+end
+
 return {
 
-  ssh_domains =
-  {
-    {
-      name = 'reServer',
-      remote_address = 'reServer.flamingsteel.net',
-      username = 'aelius',
-      assume_shell = 'Posix',
-      multiplexing = 'WezTerm',
-      local_echo_threshold_ms = 10,
-    },
-    {
-      name = 'Pyrrha',
-      remote_address = 'Pyrrha.flamingsteel.net',
-      username = 'aelius',
-      assume_shell = 'Posix',
-      multiplexing = 'WezTerm',
-      local_echo_threshold_ms = 10,
-    },
-  },
+  ssh_domains = ssh_domains,
   unix_domains = { { name = 'unix' } },
   wsl_domains = {},
 
